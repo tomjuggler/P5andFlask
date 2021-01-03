349d7b4d395a09f6ef6b3cdbb5752551f8f0fe17
Tom <tomjuggler@gmail.com>
2021-01-03 10:28:39 +0200

    1st working commit variable shared from flask to p5
    Signed-off-by: Tom <tomjuggler@gmail.com>

---
+++ main.py
@@ -0,0 +1,15 @@
+ 
+from flask import Flask, render_template      
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", variable='255')
+    
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
