c11cb9e13fa77fa911d516bdd3f5ec828185e2b5
Tom <tomjuggler@gmail.com>
2021-01-13 16:16:04 +0200

    added loading image from static folder
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- static/sketch.js
+++ static/sketch.js
@@ -1,12 +1,18 @@
+function preload() {
+  // preload() runs once
+  img = loadImage('static/Axel_12.jpg');
+}
+
 function setup() {
     createCanvas(window.innerHeight, window.innerWidth);
   }
   
   function draw() {
-    if (mouseIsPressed) {
-      fill(light, 0, 0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
+    image(img, 0, 0, width, height);
+    // if (mouseIsPressed) {
+    //   fill(light, 0, 0);
+    // } else {
+    //   fill(255);
+    // }
+    // ellipse(mouseX, mouseY, 80, 80);
   }
