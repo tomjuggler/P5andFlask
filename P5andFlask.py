394cf775811f6b4d5fc4a6c5a7ad2ceab71a251e
Tom <tomjuggler@gmail.com>
2021-04-18 20:23:32 +0200

    more directory changes for server
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- P5andFlask.py
+++ P5andFlask.py
@@ -42,7 +42,8 @@ except Exception:
         fileSave = os.path.join(dir_to_save, f"{frame}.gif")
         imageObject.save(os.path.join(fileSave))
         #run bash script to change to .jpg and move to correct directory...
-        subprocess.Popen(['./convert.sh', fileSave])
+        # subprocess.Popen(['./convert.sh', fileSave])
+        subprocess.Popen(['./var/www/P5andFlask/convert.sh', fileSave])
 
 def getPattern(pat, type):
     right = pat[pat.rfind("/"):]
@@ -117,7 +118,7 @@ def changePattern(pattern):
             fileSave = os.path.join(dir_to_save, f"{pattern}{frame}.gif") #unique name per image pattern frame
             imageObject.save(os.path.join(fileSave))
             #run bash script to change to .png and move to correct directory...
-            process = subprocess.Popen(['./convert.sh', fileSave])
+            process = subprocess.Popen(['.var/www/P5andFlask/convert.sh', fileSave])
             process.wait() # is this necessary? 
         numFrames = imageObject.n_frames
         return numFrames
