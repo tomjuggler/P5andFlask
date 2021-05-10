1459d714b8c9db4c8755798e96fd7d2414aacf80
Tom <tomjuggler@gmail.com>
2021-05-10 19:19:47 +0200

    changed path deleted all images so we can see added .gitignore
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- P5andFlask.py
+++ P5andFlask.py
@@ -13,9 +13,10 @@ import json
 from bs4 import BeautifulSoup
 
 # from subprocess import Popen
-absFilePath = os.path.dirname(os.path.abspath(__file__))
+# absFilePath = os.path.dirname(os.path.abspath(__file__))
 # dir_to_save = Path("/var/www/P5andFlask/static/")
-dir_to_save = Path(absFilePath + "/static/") #local version for tests
+# dir_to_save = Path(absFilePath + "/static/") #local version for tests
+dir_to_save = ('static')
 
 #patterns:
 threeBall = []
@@ -37,7 +38,7 @@ except Exception:
     absFilePath = os.path.dirname(os.path.abspath(__file__))
     for frame in range(0,imageObject.n_frames):
         imageObject.seek(frame)
-        dir_to_save = Path(absFilePath + "/static/") #local
+        # dir_to_save = Path(absFilePath + "/static/") #local
         # dir_to_save = Path("/var/www/P5andFlask/static/")
         print(dir_to_save)
         fileSave = os.path.join(dir_to_save, f"{frame}.gif")
